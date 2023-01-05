a76ecb72a42e2001d6551c01bbe325f9d4fb77a7
Bryan English <benglish@the-jci.org>
2023-01-05 13:14:55 -0500

    Fix depreciated and warning level code

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -151,7 +151,7 @@ class DateTime extends \DateTime implements DateTimeLinkedModelInterface
 		call_user_func_array(array($this,'parent::setISODate'),func_get_args());
 	}
 
-	public function setTime($hour, $minute, $second=null)
+	public function setTime($hour, $minute, $second=null, $microsecond=null)
 	{
 		$this->flag_dirty();
 		call_user_func_array(array($this,'parent::setTime'),func_get_args());
